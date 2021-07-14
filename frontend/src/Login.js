62895ef2d7d1d5b174d9b0ad9eacc1eba2ebceb3
ltamas <ltamas420@gmail.com>
2021-07-14 11:01:45 +0200

    minor styling changes

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -16,39 +16,54 @@ const Login = () => {
   };
 
   return (
-    <form>
-      <div></div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label"></label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-        />
-      </div>
+    <div className="container">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col">
+          <form>
+            <div></div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="exampleInputPassword1"
+                className="form-label"
+              ></label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+              />
+            </div>
 
-      <button type="button" className="btn btn-primary" onClick={loginPrep}>
-        Login
-      </button>
-    </form>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={loginPrep}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="col"></div>
+      </div>
+    </div>
   );
 };
 
