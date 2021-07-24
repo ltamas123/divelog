c4a1a8a4c1df312146270c928311c80b1d4821e0
ltamas <ltamas420@gmail.com>
2021-07-24 15:37:27 +0200

    add map in the popup

--- frontend/src/Components/SendDives.js
+++ frontend/src/Components/SendDives.js
@@ -25,41 +25,47 @@ const SendDives = ({ setDives, dives }) => {
     },
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="latitude">latitude</label>
-      <input id="latitude" type="text" {...formik.getFieldProps('latitude')} />
-      {formik.touched.latitude && formik.errors.latitude ? (
-        <div>{formik.errors.latitude}</div>
-      ) : null}
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="latitude">latitude</label>
+        <input
+          id="latitude"
+          type="text"
+          {...formik.getFieldProps('latitude')}
+        />
+        {formik.touched.latitude && formik.errors.latitude ? (
+          <div>{formik.errors.latitude}</div>
+        ) : null}
 
-      <label htmlFor="long">longitude</label>
-      <input
-        id="longitude"
-        type="text"
-        {...formik.getFieldProps('longitude')}
-      />
-      {formik.touched.longitude && formik.errors.longitude ? (
-        <div>{formik.errors.longitude}</div>
-      ) : null}
+        <label htmlFor="long">longitude</label>
+        <input
+          id="longitude"
+          type="text"
+          {...formik.getFieldProps('longitude')}
+        />
+        {formik.touched.longitude && formik.errors.longitude ? (
+          <div>{formik.errors.longitude}</div>
+        ) : null}
 
-      <label htmlFor="depth">depth</label>
-      <input id="depth" type="number" {...formik.getFieldProps('depth')} />
-      {formik.touched.depth && formik.errors.depth ? (
-        <div>{formik.errors.depth}</div>
-      ) : null}
+        <label htmlFor="depth">depth</label>
+        <input id="depth" type="number" {...formik.getFieldProps('depth')} />
+        {formik.touched.depth && formik.errors.depth ? (
+          <div>{formik.errors.depth}</div>
+        ) : null}
 
-      <label htmlFor="duration">duration</label>
-      <input
-        id="duration"
-        type="number"
-        {...formik.getFieldProps('duration')}
-      />
-      {formik.touched.duration && formik.errors.duration ? (
-        <div>{formik.errors.duration}</div>
-      ) : null}
+        <label htmlFor="duration">duration</label>
+        <input
+          id="duration"
+          type="number"
+          {...formik.getFieldProps('duration')}
+        />
+        {formik.touched.duration && formik.errors.duration ? (
+          <div>{formik.errors.duration}</div>
+        ) : null}
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
 
